d2ca7c907fd50e5e004e2a71efaa280ed52ebd73
Bethany Priode <bpriode@gmail.com>
2017-10-05 14:37:26 -0400

    created landing page and AllItems Component

---
+++ app/javascript/packs/src/components/AllItems.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+
+export default class AllItems extends Component{
+  render(){
+    return(
+      <div className="">
+      
+      </div>
+    )
+  }
+}
--- app/javascript/packs/src/components/App.js
+++ app/javascript/packs/src/components/App.js
@@ -3,7 +3,35 @@ import React, {Component} from 'react';
 export default class App extends Component{
   render(){
     return(
-      <p>This works!!!</p>
+      <div className="d-flex flex-row">
+      <ul className="nav flex-column">
+      <li className="nav-item">
+        <a className="nav-link" href="#">All Items</a>
+      </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Furniture</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Kitchen</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Decor</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Games</a>
+        </li>
+    </ul>
+
+    <div className="jumbotron ml-3">
+      <h2 className="display-2">It All Has To Go!</h2>
+      <p className="lead">Help out the Atlanta Campus and yourself by looking through and purchasing the pieces that surrounded you during a life-changing time.</p>
+      <hr className="my-4"></hr>
+      <p>Remember the desk you sat at while learning to code? It can be yours!</p>
+      <p className="lead">
+        <a className="btn btn-primary btn-lg" href="#" role="button">See All Items</a>
+      </p>
+    </div>
+    </div>
     )
   }
 }
