e5dee16b31c65734df89d3e3d69fcac933a57ebf
Robby Dore <rdore88@gmail.com>
2017-10-08 19:29:35 -0400

    Created user tests and corrected users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
+  before_action :authorize!, only: [:destroy]
 
   def create
     @user = User.find_by(name: params[:name])
-    if @user && user.authenticate([:password])
+    if @user && @user.authenticate(params[:password])
       render json: {user_id: @user.id}
     else
       render json: {message: "Name or password not correct", status: :bad_request}
