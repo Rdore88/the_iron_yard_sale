292d225192a0ee572d552d9c374bc57fe56fe48c
Isaac Hardy <isaac@isaachardy.io>
2017-10-18 16:38:35 -0400

    added tooltips to store list so titles can be viewed if too long

--- app/javascript/packs/src/components/AllItems/AllItems.js
+++ app/javascript/packs/src/components/AllItems/AllItems.js
@@ -2,10 +2,23 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import {deleteInventoryItem} from '../../actions/index';
+import { Tooltip } from 'reactstrap';
 
 class AllItems extends Component{
   constructor(props) {
     super(props)
+
+    this.state = {
+      currentTooltip: '',
+      isToolTipOpen: false
+    }
+  }
+
+  toggle = (id) => {
+    this.setState({
+      currentTooltip: String(id),
+      isToolTipOpen: !this.state.isToolTipOpen
+    })
   }
   generateInventoryList = (filter) => {
     let filteredItems = this.props.inventoryItems.inventory.filter(item => {
@@ -14,9 +27,16 @@ class AllItems extends Component{
 
     let listedItems = filteredItems.map((item, index) => {
       return (
-        <li key={index}>
+        <li key={index} className="ml-4">
           <div className="d-flex justify-content-center">
-            <h4 className="card-title mr-5">{item.title}</h4>
+            <h5 id={"tooltip-" + index} className="card-title mr-5">{item.title.length > 20 ? item.title.substring(0, 20) + '...' : item.title}</h5>
+            <Tooltip
+              placement="top"
+              isOpen={this.state.isToolTipOpen && this.state.currentTooltip === String(index) ? true : false}
+              target={"tooltip-" + index}
+              toggle={() => this.toggle(index)}>
+              {item.title}
+            </Tooltip>
             <h4 className="card-title text-primary">${item.price}</h4>
           </div>
           <div className="item-card card bg-dark rounded-top" style={{width: "20rem", height: "20rem"}}>
@@ -53,15 +73,21 @@ class AllItems extends Component{
 
   render(){
     let items;
-
     switch (this.props.filter) {
       case "All":
         items = this.props.inventoryItems.inventory.map((item, index) => {
           return (
-            <li key={index}>
+            <li key={index} className="ml-4">
               <div className="d-flex justify-content-center">
-                <h4 className="card-title mr-5">{item.title}</h4>
-                <h4 className="card-title text-primary">${item.price}</h4>
+                <h5 id={"tooltip-" + index} className="card-title mr-5">{item.title.length > 20 ? item.title.substring(0, 20) + '...' : item.title}</h5>
+                <Tooltip
+                  placement="top"
+                  isOpen={this.state.isToolTipOpen && this.state.currentTooltip === String(index) ? true : false}
+                  target={"tooltip-" + index}
+                  toggle={() => this.toggle(index)}>
+                  {item.title}
+                </Tooltip>
+                <h6 className="card-title text-primary">${item.price}</h6>
               </div>
               <div className="item-card card bg-dark rounded-top" style={{width: "20rem", height: "20rem"}}>
                 <img className="card-img-top" src="..." alt=""></img>
