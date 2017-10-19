381b359e2a60f1362b583a8ff9fba16cba6b44de
Robby Dore <rdore88@gmail.com>
2017-10-19 19:09:01 -0400

    Passed tests for new mailer

--- spec/mailers/order_created_mailer_spec.rb
+++ spec/mailers/order_created_mailer_spec.rb
@@ -1,5 +1,15 @@
 require "rails_helper"
 
 RSpec.describe OrderCreatedMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:item) {Item.create!(title: "desk", description: "The best desk ever", price: 20.99, category: "furniture", quantity: 50)}
+  let(:order) {Order.create!(name_of_buyer: "imani", email_of_buyer: "imani@olivegrove.com", phone_of_buyer: "404-555-5555", quantity: 10, item_id: item.id)}
+  let(:mail) {OrderCreatedMailer.order_created(order)}
+
+  it "exits" do
+      assert OrderCreatedMailer
+  end
+
+  it "render the subject with correct user" do
+    expect(mail.subject).to eq("Thank you imani for your order")
+  end
 end
